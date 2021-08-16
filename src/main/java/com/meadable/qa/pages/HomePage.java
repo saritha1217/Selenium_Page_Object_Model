3512afc80ec35635796054502d80f1ba9505bd8d
saritha1217 <61355921+saritha1217@users.noreply.github.com>
2021-08-15 22:46:25 -0500

    Update HomePage.java
    updated code

--- src/main/java/com/meadable/qa/pages/HomePage.java
+++ src/main/java/com/meadable/qa/pages/HomePage.java
@@ -13,7 +13,9 @@ public class HomePage extends TestBase {
 	//other user pushed some more code in same file
 	//@FindBy()
 	//WebElement popup_title;
-	
+	public void testMethod(){
+		//This method will used to start the test
+	}
 	//@FindBy()
 	//WebElement popup_text1;
 	
